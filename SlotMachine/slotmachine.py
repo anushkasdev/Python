1b52d04815856a5e68d71e223ef1bcb5d4667d95
anushkasdev <anushka.soni.dev@gmail.com>
2025-11-01 23:53:43 +0530

    first commit

---
+++ SlotMachine/slotmachine.py
@@ -0,0 +1,47 @@
+MAX_LINES = 4
+MAX_BET = 50
+MIN_BET = 1
+def getAmount(): 
+  while True : 
+    amount = input("Please enter the amount for win : $")
+    if amount.isdigit():
+      amount = int(amount)
+      if amount > 0:
+        break
+      else:
+        print("Please enter a valid number.")
+    else:
+      print("Please enter a valid amount only.")
+  return amount
+
+def getLines(): 
+  while True : 
+    lines = input("Please enter the number of lines : ")
+    if lines.isdigit():
+      lines = int(lines)
+      if 1 <=lines<= MAX_LINES:
+        break
+      else:
+        print("Please enter a valid number.")
+    else:
+      print("Please enter a valid line from 1 to " + int(MAX_LINES)+ ".")
+  return lines
+
+def getBet(): 
+  while True : 
+    bet = input("Please enter the amount for Betting : $")
+    if bet.isdigit():
+      bet = int(bet)
+      if MIN_BET<=bet<=MAX_BET:
+        break
+      else:
+        print(f"Please enter a valid bet in the range from .")
+    else:
+      print("Please enter a valid amount only.")
+  return bet
+
+def main():
+  amount = getAmount()
+  lines = getLines()
+  print(lines, amount)
+main()
