4d7490af1bef2166f4bb957c4dde8d1c8185882b
anushkasdev <anushka.soni.dev@gmail.com>
2025-11-08 23:59:17 +0530

    Visual slot representation

--- SlotMachine/slotmachine.py
+++ SlotMachine/slotmachine.py
@@ -30,6 +30,14 @@ def get_slot_machine_spins(rows, cols, symbols):
     columns.append(column)
   return columns
 
+
+def print_slot_machine(columns):
+  for row in range(len(columns[0])):
+    for i, column in enumerate(columns):
+      if i != len(columns) - 1:
+        print(column[row], "|")
+      else:
+        print(column[row])
 def getAmount(): 
   while True : 
     amount = input("Please enter the amount for deposit : $")
