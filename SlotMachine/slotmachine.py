f79ddc7af8a0247644b78d0520c5c92b2d52671e
anushkasdev <anushka.soni.dev@gmail.com>
2025-11-10 23:57:49 +0530

    fixing the slot direction

--- SlotMachine/slotmachine.py
+++ SlotMachine/slotmachine.py
@@ -35,9 +35,10 @@ def print_slot_machine(columns):
   for row in range(len(columns[0])):
     for i, column in enumerate(columns):
       if i != len(columns) - 1:
-        print(column[row], "|")
+        print(column[row], end = "|")
       else:
-        print(column[row])
+        print(column[row], end = "")
+    print()
 def getAmount(): 
   while True : 
     amount = input("Please enter the amount for deposit : $")
